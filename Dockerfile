244afec6b1741d9b9f28c865d3442cc936b27ad9
aquova <abrick912@gmail.com>
2022-04-27 12:11:51 -0500

    Switch from py-cord to master branch of discord.py

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,7 @@ RUN pip3 install mypy
 
 COPY setup.py /tmp
 COPY commonbot /tmp/commonbot
+RUN git clone https://github.com/Rapptz/discord.py /tmp/discord.py
+RUN pip3 install -U /tmp/discord.py
 RUN python3 /tmp/setup.py bdist_wheel
-RUN pip3 install /tmp/dist/commonbot-1.7.3.2-py3-none-any.whl
+RUN pip3 install /tmp/dist/commonbot-2.0.0b1-py3-none-any.whl
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = "1.7.3.2"
+VERSION = "2.0.0-beta1"
 DESCRIPTION = "aquova's common bot package"
 LONG_DESCRIPTION = "A package containing common functionality for my Discord bots"
 
@@ -13,5 +13,5 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     package_data={"commonbot": ["py.typed"]},
-    install_requires=["py-cord==1.7.3"]
+    install_requires=[]
 )
